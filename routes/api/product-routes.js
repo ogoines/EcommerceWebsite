d88a47f1ecff074d23aa417d015874ecc90e584b
OGrunner <orshagoines@gmail.com>
2022-09-22 00:38:10 -0400

    modified:   routes/api/product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,8 +5,8 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', async (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  // finds all products
+  //Includes its associated Category and Tag data
   try {
     const productData = await Product.findAll({
       include: [{ model: Category }, { model: Tag }, { model: ProductTag },],
